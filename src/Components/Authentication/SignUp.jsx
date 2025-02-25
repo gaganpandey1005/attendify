275a32f1511838a8e9cebbcd0e0cce331a68fd26
gagan pandey <149556650+gaganpandey1005@users.noreply.github.com>
2025-02-25 17:33:55 +0530

    sign up and sign in commit

--- src/Components/Authentication/SignUp.jsx
+++ src/Components/Authentication/SignUp.jsx
@@ -36,7 +36,7 @@ const SignUp = () => {
 
     try {
       const response = await axios.post(
-        "https://attendify-backend-szi8.onrender.com",
+        "https://attendify-backend-szi8/api/register",
         formData,
         { headers: { "Content-Type": "application/json" } }
       );
